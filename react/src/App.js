ff261ff6eaf6b37ca4f06a2538bb6a69a785e149
Johnathan Bell <enigma.0Z@gmail.com>
2022-12-07 12:34:23 -0500

    First pass porting existing app over to react

--- react/src/App.js
+++ react/src/App.js
@@ -1,26 +1,110 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './style/main.css';
+import { renderUi } from './scripts/ui.js'
+import { BrowserRouter } from 'react-router-dom'
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const head = document.querySelector("head");
+    const adsense = document.createElement("script");
+
+    adsense.setAttribute("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6012158634418176");
+    adsense.setAttribute('async', '')
+    adsense.setAttribute('crossorigin', 'anonymous')
+    head.appendChild(adsense);
+
+    // const oldui = document.createElement("script");
+    // oldui.setAttribute("src", "/v2/scripts/ui.js");
+    // oldui.setAttribute('type', 'module')
+    // head.appendChild(oldui);
+
+    renderUi()
+
+    return () => {
+      head.removeChild(adsense);
+      // head.removeChild(oldui);
+    };
+  })
+  // renderUi()
   return (
     <BrowserRouter basename='/v2'>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div class="background-color"></div>
+        <div class="background-image"></div>
+        <div class="section"><h1>
+          Sandwich Calculator
+        </h1></div>
+        <div id="sandwich" class="content"></div>
+        <div id="todo" class="content">
+          <h2>Todo</h2>
+          <ul><li>
+            Multiplayer mode
+          </li><li>
+            Reverse mode -- Specify powers & types and generate a sandwich that partially or fully fulfils this condition
+          </li><li>
+            Option for less than perfection in ingredients placed
+          </li><li>
+            Penalty for sandwich failure
+          </li><li>
+            Penalty for too many ingredients (>13 in Single Player)
+          </li><li>
+            Add known recipes
+          </li><li>
+            Type/power/flavor filtering or searching
+          </li><li>
+            Recipe suggestions based on sandwich goals
+          </li><li>
+            Database of useful DIY sandwiches
+          </li><li>
+            Add ingredient pics / custom select box? 
+          </li><li>
+            Rewrite the UI in react &lt;_&lt;
+          </li></ul>
+        </div>
+        <div id="notes" class="content">
+          <h2>Notes</h2>
+          <ul><li>
+            Yes I know it's ugly. I'm working on it after I get the math close enough to right
+          </li><li>
+            Known recipes powers are hardcoded and they don't follow the same values as DIY ones.
+          </li></ul>
+        </div>
+        <div id="known issues" class="content">
+          <h2>Known Issues</h2>
+          <ul><li>
+            Defined (in-game) recpies are goofy and don't calc correctly
+          </li><li>
+            Flavor bonuses only occur or get calculated sometimes.  Unknown why / when.
+          </li></ul>
+        </div>
+        <div class="section">
+          <span class="caption">
+            <a href="https://www.youtube.com/channel/UCMVAWJUY88YlJlX88B0siDQ">YouTube</a> |&nbsp;
+            <a href="https://twitter.com/enigma_0z">Twitter</a> |&nbsp;
+            <a href="https://discord.gg/Wyemk6G28z">Discord</a> |&nbsp;
+            <a href="https://www.patreon.com/enigmaplaysgames">Patreon</a> |&nbsp;
+            <a href="https://enigma-plays-live.myspreadshop.com/">Merch</a>
+          </span>
+        </div>
+        <div class="section">
+          <img class="header" src="img/enigma.png"/>
+        </div>
+        {/* <div class="section">
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6012158634418176"
+            crossorigin="anonymous"></script>
+
+          <ins class="adsbygoogle"
+              style="display:block"
+              data-ad-client="ca-pub-6012158634418176"
+              data-ad-slot="8019985505"
+              data-ad-format="auto"
+              data-full-width-responsive="true"></ins>
+          <script>
+              (adsbygoogle = window.adsbygoogle || []).push({});
+          </script> */}
+        </div>
     </BrowserRouter>
   );
 }
---
+++ react/src/scripts/calc.js
@@ -0,0 +1,338 @@
+const xmlHttpReq = new XMLHttpRequest();
+
+xmlHttpReq.open("GET", 'data/sandwich.json', false);
+xmlHttpReq.send(null);
+export const SandwichData = JSON.parse(xmlHttpReq.responseText);
+
+xmlHttpReq.open("GET", 'data/recipe.json', false);
+xmlHttpReq.send(null);
+export const RecipeData = JSON.parse(xmlHttpReq.responseText);
+
+
+const PokemonTypes = [
+  'Normal',
+  'Fighting',
+  'Flying',
+  'Poison',
+  'Ground',
+  'Rock',
+  'Bug',
+  'Ghost',
+  'Steel',
+  'Fire',
+  'Water',
+  'Grass',
+  'Electric',
+  'Psychic',
+  'Ice',
+  'Dragon',
+  'Dark',
+  'Fairy',
+]
+
+const MealPowers = [
+  'Egg',
+  'Catching',
+  'Exp.',
+  'Item Drop',
+  'Raid',
+  'Title',
+  'Sparkling',
+  'Humungo',
+  'Teensy',
+  'Encounter',
+]
+
+const Tastes = [
+  'Sweet',
+  'Salty',
+  'Sour',
+  'Bitter',
+  'Hot',
+]
+
+function templateResult() {
+  return {
+    taste: {
+      'Sweet': 0,
+      'Hot': 0,
+      'Bitter': 0,
+      'Sour': 0,
+      'Salty': 0,
+    },
+    power: {
+      'Egg': 0,
+      'Catching': 0,
+      'Exp.': 0,
+      'Item Drop': 0,
+      'Raid': 0,
+      'Title': 0,
+      'Sparkling': 0,
+      'Humungo': 0,
+      'Teensy': 0,
+      'Encounter': 0,
+    },
+    type: {
+      'Normal': 0,
+      'Fire': 0,
+      'Water': 0,
+      'Electric': 0,
+      'Grass': 0,
+      'Ice': 0,
+      'Fighting': 0,
+      'Poison': 0,
+      'Ground': 0,
+      'Flying': 0,
+      'Psychic': 0,
+      'Bug': 0,
+      'Rock': 0,
+      'Ghost': 0,
+      'Dragon': 0,
+      'Dark': 0,
+      'Steel': 0,
+      'Fairy': 0,
+    }
+  }
+}
+
+export function sortValueName(a, b) {
+  return (b.value - a.value) + (a.name < b.name ? -1 : 1)
+}
+
+export function sortValueTaste(a, b) {
+  return (b.value - a.value) + (Tastes.indexOf(a.name) < Tastes.indexOf(b.name) ? -1 : 1)
+}
+
+export function sortValueType(a, b) {
+  return (b.value - a.value) + (PokemonTypes.indexOf(a.name) < PokemonTypes.indexOf(b.name) ? -1 : 1)
+}
+
+export function sortValuePower(a, b) {
+  return (b.value - a.value) + (MealPowers.indexOf(a.name) < MealPowers.indexOf(b.name) ? -1 : 1)
+}
+
+export function sortAttributes(object, sortFn = sortValueName) {
+  return Object.keys(object)
+    .map((key) => { return { name: key, value: object[key] } })
+    .sort(sortFn)
+}
+
+export function lookupIngredientByName(name) {
+  return SandwichData.ingredients.find((ingredient) => ingredient.name.toLowerCase() === name.toLowerCase())
+}
+
+export function lookupSeasoningByName(name) {
+  return SandwichData.seasonings.find((seasoning) => seasoning.name.toLowerCase() === name.toLowerCase())
+}
+
+export function calculateLevel(power, type) {
+  if (power.value >= 100 && power.value < 2000 && type.value > 180) {
+    return 2
+  } else {
+    return 1
+  }
+}
+
+
+export function sumComponents(components, lookupFunction) {
+  const Result = templateResult()
+  // Add everything together
+  for (let component of components) {
+    const ComponentData = lookupFunction(component)
+    const Multiplier = ComponentData.max !== undefined ? ComponentData.max : 1
+    for (let key of Object.keys(ComponentData.taste)) {
+      Result.taste[key] += ComponentData.taste[key] * Multiplier
+    }
+    for (let key of Object.keys(ComponentData.power)) {
+      Result.power[key] += ComponentData.power[key] * Multiplier
+    }
+    for (let key of Object.keys(ComponentData.type)) {
+      Result.type[key] += ComponentData.type[key] * Multiplier
+    }
+  }
+
+  return Result
+}
+
+export function sumComponentData(components) {
+  const Result = templateResult()
+
+  for (let component of components) {
+    for (let key of Object.keys(component.taste)) {
+      Result.taste[key] += component.taste[key]
+    }
+    for (let key of Object.keys(component.power)) {
+      Result.power[key] += component.power[key]
+    }
+    for (let key of Object.keys(component.type)) {
+      Result.type[key] += component.type[key]
+    }
+  }
+
+  return Result
+}
+
+export function addFlavorResult(sandwich) {
+  // Taste Powers
+  const SortedTastes = sortAttributes(sandwich.taste, sortValueTaste)
+  console.log(SortedTastes[0], SortedTastes[1])
+
+  let bonus = 100;
+  if (
+    SortedTastes[0].name === 'Sour' && SortedTastes[1].name === 'Sweet'
+    || SortedTastes[0].name === 'Sweet' && SortedTastes[1].name === 'Sour'
+  ) {
+    sandwich.power['Catching'] += bonus
+  } else if (
+    SortedTastes[0].name === 'Bitter' && SortedTastes[1].name === 'Salty'
+    || SortedTastes[0].name === 'Salty' && SortedTastes[1].name === 'Bitter'
+  ) {
+    sandwich.power['Exp'] += bonus
+  } else if (
+    SortedTastes[0].name === 'Hot' && SortedTastes[1].name === 'Sweet'
+    || SortedTastes[0].name === 'Sweet' && SortedTastes[1].name === 'Hot'
+  ) {
+    sandwich.power['Raid'] += bonus
+  } else if (SortedTastes[0].name === 'Sweet') {
+    sandwich.power['Egg'] += bonus
+  } else if (SortedTastes[0].name === 'Hot') {
+    sandwich.power['Humungo'] += bonus
+  } else if (SortedTastes[0].name === 'Bitter') {
+    sandwich.power['Item Drop'] += bonus
+  } else if (SortedTastes[0].name === 'Sour') {
+    sandwich.power['Teensy'] += bonus
+  } else if (SortedTastes[0].name === 'Salty') {
+    sandwich.power['Encounter'] += bonus
+  }
+
+  return sandwich
+}
+
+export function combineComponents(ingredients, seasonings) {
+  const Result = {
+    taste: {},
+    power: {},
+    type: {}
+  }
+
+  // Combine tastes
+  for (let key of Object.keys(ingredients.taste)) {
+    if (Result.taste[key] === undefined) {
+      Result.taste[key] = ingredients.taste[key]
+    } else {
+      Result.taste[key] += ingredients.taste[key]
+    }
+  }
+
+  for (let key of Object.keys(seasonings.taste)) {
+    if (Result.taste[key] === undefined) {
+      Result.taste[key] = seasonings.taste[key]
+    } else {
+      Result.taste[key] += seasonings.taste[key]
+    }
+  }
+
+  // Combine powers
+  for (let key of Object.keys(ingredients.power)) {
+    if (Result.power[key] === undefined) {
+      Result.power[key] = ingredients.power[key]
+    } else {
+      Result.power[key] += ingredients.power[key]
+    }
+  }
+
+  for (let key of Object.keys(seasonings.power)) {
+    if (Result.power[key] === undefined) {
+      Result.power[key] = seasonings.power[key]
+    } else {
+      Result.power[key] += seasonings.power[key]
+    }
+  }
+
+  // Combine types
+  for (let key of Object.keys(ingredients.type)) {
+    if (Result.type[key] === undefined) {
+      Result.type[key] = ingredients.type[key]
+    } else {
+      Result.type[key] += ingredients.type[key]
+    }
+  }
+
+  for (let key of Object.keys(seasonings.type)) {
+    if (Result.type[key] === undefined) {
+      Result.type[key] = seasonings.type[key]
+    } else {
+      Result.type[key] += seasonings.type[key]
+    }
+  }
+
+  return Result
+}
+
+export function calculateSandwich(ingredients, seasonings) {
+  const Sandwich = {
+    powers: [ ]
+  }
+
+  const IngredientSum = sumComponents(ingredients, lookupIngredientByName)
+  const SeasoningSum = sumComponents(seasonings, lookupSeasoningByName)
+  const SandwichSum = combineComponents(IngredientSum, SeasoningSum)
+
+  addFlavorResult(SandwichSum)
+
+  const SortedTaste = Object.keys(SandwichSum.taste)
+    .map( (key) => { return {name: key, value: SandwichSum.taste[key]} })
+    .sort(sortValueTaste)
+
+  const SortedPower = Object.keys(SandwichSum.power)
+    .map( (key) => { return {name: key, value: SandwichSum.power[key]} })
+    .sort(sortValuePower)
+
+  const SortedType = Object.keys(SandwichSum.type)
+    .map( (key) => { return {name: key, value: SandwichSum.type[key]} })
+    .sort(sortValueType)
+
+  let type = []
+
+  // Do type sorting and check for monotype
+  console.log('top type', SortedType[0].value)
+  if (SortedType[0].value < 280) {
+    type = [0, 2, 1]
+  } else if (SortedType.value < 480) {
+    type = [0, 0, 2]
+  } else {
+    console.log('monotype sandwich')
+    type = [0, 0, 0]
+  }
+
+  // Calculate initial levels
+  let levels = [
+    calculateLevel(SortedPower[0], SortedType[type[0]]),
+    calculateLevel(SortedPower[1], SortedType[type[1]]),
+    calculateLevel(SortedPower[2], SortedType[type[2]]),
+  ]
+
+  // Do Herba Mystica overrides for level
+  let herbaMysticaTotal = 0;
+  for (let seasoning of seasonings) {
+    if (seasoning.toLowerCase().endsWith('herba mystica')) herbaMysticaTotal += 1
+  }
+
+  if (herbaMysticaTotal == 1) {
+    if (levels[0] < 2) levels[0] = 2 
+    if (levels[1] < 2) levels[1] = 2 
+  } else if (herbaMysticaTotal > 1) {
+    if (levels[0] < 3) levels[0] = 3 
+    if (levels[1] < 3) levels[1] = 3 
+    if (levels[2] < 3) levels[2] = 3 
+  }
+
+  // Types 2 and 3 are switched vs power order
+  Sandwich.powers.push(
+    {name: SortedPower[0].name, type: SortedType[type[0]].name, level: levels[0]},
+    {name: SortedPower[1].name, type: SortedType[type[1]].name, level: levels[1]},
+    {name: SortedPower[2].name, type: SortedType[type[2]].name, level: levels[2]},
+  )
+
+  return Sandwich
+}

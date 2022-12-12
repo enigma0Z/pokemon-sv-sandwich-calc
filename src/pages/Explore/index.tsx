16566d4dcbd39588855d29d5bf1fac859d6b1f7d
enigma0Z <enigma.0ZA@gmail.com>
2022-12-12 17:30:58 -0500

    First pass at ingredient explorer

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -1,14 +1,29 @@
+import { Box } from '@mui/material'
 import { useEffect } from 'react'
+import IngredientDetail from '../../components/IngredientDetail'
+import { Ingredients, Seasonings } from '../../data/Cookbooks'
 
 export default function Explore() {
   useEffect(() => {
     document.title = "Sandwich Calculator: Explore"
   }, [])
+
+  let ingredients = Ingredients.map(x => <IngredientDetail kind='ingredient' key={x.name} name={x.name}/>)
+  let seasonings = Seasonings.map(x => <IngredientDetail kind='seasoning' key={x.name} name={x.name}/>)
   return (
     <>
-      <div className="section">
-        <h2>COMING SOON</h2>
-      </div>
+      <Box className="section">
+        <h2>Ingredients</h2>
+      </Box>
+      <Box display='flex' flexDirection={'row'} flexWrap='wrap'>
+        {ingredients}
+      </Box>
+      <Box className="section">
+        <h2>Seasonings</h2>
+      </Box>
+      <Box display='flex' flexDirection={'row'} flexWrap='wrap'>
+        {seasonings}
+      </Box>
     </>
   )
 }

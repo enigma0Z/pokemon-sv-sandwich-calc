8690263af2946b0dfaa9f9ce6c168da7905367d5
enigma0Z <enigma.0ZA@gmail.com>
2022-12-12 19:03:46 -0500

    First pass at explorer page

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -1,28 +1,76 @@
-import { Box } from '@mui/material'
-import { useEffect } from 'react'
+import { Autocomplete, Box, TextField } from '@mui/material'
+import { useEffect, useState } from 'react'
 import IngredientDetail from '../../components/IngredientDetail'
 import { Ingredients, Seasonings } from '../../data/Cookbooks'
+import { PokemonTypes, MealPowers, Tastes } from '../../data/calc'
+import { Ingredient } from '../../data/Cookbook'
 
 export default function Explore() {
   useEffect(() => {
     document.title = "Sandwich Calculator: Explore"
   }, [])
 
-  let ingredients = Ingredients.map(x => <IngredientDetail kind='ingredient' key={x.name} name={x.name}/>)
-  let seasonings = Seasonings.map(x => <IngredientDetail kind='seasoning' key={x.name} name={x.name}/>)
+  const [filter, setFilter] = useState<string[]>([])
+
+  const isVisible = (ingredient: Ingredient): boolean => {
+    if (filter.length === 0) {
+      return true
+    } else {
+      for (let f of filter) {
+        const inTaste = f in ingredient.taste
+        const inPower = f in ingredient.power
+        const inType = f in ingredient.type
+
+        if (!inTaste && !inPower && !inType) {
+          return false
+        }
+      }
+      return true
+    }
+  }
+
   return (
     <>
+      <Box display='flex' flexDirection='row'>
+        <Autocomplete 
+          multiple
+          filterSelectedOptions
+          autoHighlight
+          autoSelect
+          fullWidth
+          size="small"
+          options={[...Tastes, ...MealPowers, ...PokemonTypes]} 
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Filter Ingredients"
+              placeholder=""
+            />
+          )}
+          onChange={(event, value) => {
+            setFilter(value)
+          }}
+        />
+      </Box>
       <Box className="section">
         <h2>Ingredients</h2>
       </Box>
       <Box display='flex' flexDirection={'row'} flexWrap='wrap'>
-        {ingredients}
+        {Ingredients.map(x => 
+          <Box display={isVisible(x) ? 'inline' : 'none'}>
+            <IngredientDetail kind="ingredient" name={x.name} />
+          </Box>
+        )}
       </Box>
       <Box className="section">
         <h2>Seasonings</h2>
       </Box>
       <Box display='flex' flexDirection={'row'} flexWrap='wrap'>
-        {seasonings}
+        {Seasonings.map(x => 
+          <Box display={isVisible(x) ? 'inline' : 'none'}>
+            <IngredientDetail kind="seasoning" name={x.name} />
+          </Box>
+        )}
       </Box>
     </>
   )

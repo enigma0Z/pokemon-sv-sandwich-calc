d9a412365a0ae246ae712b500bef6b02b7b70c7f
enigma0Z <enigma.0ZA@gmail.com>
2022-12-30 12:33:28 -0500

    Add nitropay anchor ad

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -49,20 +49,7 @@ export default function Layout() {
         <div className="section">
           <img className="header" src={res.img.logo} alt="enigma! logo" />
         </div>
-        <div className="section">
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6012158634418176"
-            crossOrigin="anonymous"></script>
-          {/* Bottom Banner Ad */}
-          <ins className="adsbygoogle"
-            style={{ display: 'block' }}
-            data-ad-client="ca-pub-6012158634418176"
-            data-ad-slot="8019985505"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
-          <script>
-            (adsbygoogle = window.adsbygoogle || []).push({ });
-          </script>
-        </div>
+        <Box height="100px"/>
       </ThemeProvider>
     </>
   )

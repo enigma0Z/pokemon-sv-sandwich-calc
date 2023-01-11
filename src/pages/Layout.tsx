331dceeb586922a1070ef3d0be3a18cd51a957e1
enigma0Z <enigma.0ZA@gmail.com>
2023-01-10 23:12:32 -0500

    Tweaks for more better ads

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,8 +2,10 @@ import { Box, Link, Theme, Typography, useTheme } from '@mui/material';
 import { NavLink, Outlet, useLocation } from 'react-router-dom';
 import res from '../res';
 import './Layout.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { NitroPayConfig } from '../util/NitroPayConfig';
 
+let gutterAd: any
 
 export default function Layout() {
 
@@ -19,20 +21,100 @@ export default function Layout() {
     footerImage: {
       width: '90%'
     },
+    gutter: {
+      [theme.breakpoints.up('lg')]: {
+        minWidth: '300px',
+      },
+      [theme.breakpoints.only('md')]: {
+        minWidth: '160px',
+      },
+      [theme.breakpoints.down('md')]: {
+        minWidth: '0px',
+        display: 'none'
+      }
+    },
+    layout: {
+      display: 'flex',
+      flexDirection: 'row'
+    },
     app: {
       textAlign: 'left',
-      padding: '.5em',
+      padding: '10px',
       marginLeft: 'auto',
       marginRight: 'auto',
-      maxWidth: '94em',
+      display: 'flex',
+      flexDirection: 'column',
+      [theme.breakpoints.only('xl')]: { // 1536
+        width: '1150px'
+      },
+      [theme.breakpoints.only('lg')]: { // 1200
+        width: '850px'
+      },
+      [theme.breakpoints.only('md')]: { // 900
+        width: '850px'
+      },
+      [theme.breakpoints.down('md')]: { // 900
+        width: '100%'
+      },
     }
   })
+
   const classes = styles(theme)
+
+  useEffect(() => {
+    //@ts-ignore
+    window['nitroAds'].createAd('sandwich-bottom-anchor', {
+      ...NitroPayConfig,
+      "format": "anchor",
+      "anchor": "bottom",
+      "anchorPersistClose": false,
+      "report": {
+        "enabled": true,
+        "icon": true,
+        "wording": "Report Ad",
+        "position": "top-left"
+      },
+      "mediaQuery": "(min-width: 320px) and (max-width: 899px)"
+    });
+
+  }, [])
+
+  useEffect(() => {
+    const stickyStackElement = document.getElementById('sandwich-right-gutter-sticky-stack')
+    if (stickyStackElement) {
+      if (stickyStackElement.innerHTML !== '') {
+        stickyStackElement.innerHTML = ''
+      }
+
+      //@ts-ignore
+      window['nitroAds'].createAd('sandwich-right-gutter-sticky-stack', {
+        ...NitroPayConfig,
+        "format": "sticky-stack",
+        "stickyStackLimit": 15,
+        "stickyStackSpace": 1.25,
+        "stickyStackOffset": 200,
+        "sizes": [
+          [
+            "160",
+            "600"
+          ]
+        ],
+        "report": {
+          "enabled": true,
+          "icon": true,
+          "wording": "Report Ad",
+          "position": "top-right"
+        },
+        "mediaQuery": "(min-width: 900px)"
+      })
+    }
+  })
 
   return (
     <>
       <div className="background-color"></div>
       <div className="background-image"></div>
+
       <Typography variant={'h1'} sx={classes.section}>
         Sandwich Calculator
       </Typography>
@@ -46,8 +128,11 @@ export default function Layout() {
         </Box>
       </nav>
 
-      <Box sx={classes.app}> 
-        <Outlet context={[setSearch]} />
+      <Box sx={classes.layout}>
+        <Box sx={classes.app}>
+          <Outlet context={[setSearch]} />
+        </Box>
+        <Box id={'sandwich-right-gutter-sticky-stack'} sx={classes.gutter} />
       </Box>
 
       <Box sx={classes.section}>
@@ -55,21 +140,21 @@ export default function Layout() {
         If you found a bug or have a suggestion, report it in the <Link href="https://www.reddit.com/user/enigma_0Z/comments/zfk3bz/pokemon_scarlet_violet_sandwich_calculator_bug/" target='_blank' rel="noreferrer">official reddit thread</Link>
       </Box>
       <Box sx={classes.section}>
-          <Link href="https://www.youtube.com/channel/UCMVAWJUY88YlJlX88B0siDQ">YouTube</Link> |&nbsp;
-          <Link href="https://twitter.com/enigma_0z">Twitter</Link> |&nbsp;
-          <Link href="https://discord.gg/Wyemk6G28z">Discord</Link> |&nbsp;
-          <Link href="https://www.patreon.com/enigmaplaysgames">Patreon</Link> |&nbsp;
-          <Link href="https://enigma-plays-live.myspreadshop.com/">Merch</Link>
+        <Link href="https://www.youtube.com/channel/UCMVAWJUY88YlJlX88B0siDQ">YouTube</Link> |&nbsp;
+        <Link href="https://twitter.com/enigma_0z">Twitter</Link> |&nbsp;
+        <Link href="https://discord.gg/Wyemk6G28z">Discord</Link> |&nbsp;
+        <Link href="https://www.patreon.com/enigmaplaysgames">Patreon</Link> |&nbsp;
+        <Link href="https://enigma-plays-live.myspreadshop.com/">Merch</Link>
       </Box>
       <Box sx={classes.section}>
-        <img style={{width: '80%', imageRendering: 'pixelated'}} src={res.img.logo} alt="enigma! logo" />
+        <img style={{ width: '80%', imageRendering: 'pixelated' }} src={res.img.logo} alt="enigma! logo" />
       </Box>
       <Box sx={classes.section}>
         <Link href="https://enigma0z.com/privacy.html">Privacy Policy</Link>
-        <br/>
+        <br />
         <Link component='span' data-ccpa-link={1} />
       </Box>
-      <Box sx={{...classes.section, height: '100px'}}/>
+      <Box sx={{ ...classes.section, height: '100px' }} />
     </>
   )
 }
